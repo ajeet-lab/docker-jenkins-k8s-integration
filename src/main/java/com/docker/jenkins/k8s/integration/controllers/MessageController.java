a115fbc0444c66684dba1adc7d3d25eb751494f2
AJEET KUSHWAHA <51816732+ajeet-lab@users.noreply.github.com>
2025-06-04 00:56:36 +0530

    Added controller

---
+++ src/main/java/com/docker/jenkins/k8s/integration/controllers/MessageController.java
@@ -0,0 +1,15 @@
+package com.docker.jenkins.k8s.integration.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MessageController {
+
+    @GetMapping
+    public String getMessage(){
+        return "Message is return";
+    }
+}
