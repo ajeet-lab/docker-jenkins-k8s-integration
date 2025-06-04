38dc128900c9acaffcbd2986e922c32ddd3f5a80
AJEET KUSHWAHA <51816732+ajeet-lab@users.noreply.github.com>
2025-06-04 11:22:55 +0530

    Update DockerJenkinsIntegrationApplication.java

--- src/main/java/com/docker/jenkins/k8s/integration/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/docker/jenkins/k8s/integration/DockerJenkinsIntegrationApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DockerJenkinsIntegrationApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting point here appppppp...ssssss");
+		System.out.println("Starting point here appppppp...sssssssafafa");
 		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
 	}
 }
